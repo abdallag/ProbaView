bc7f47ed511def99c665bc8c4712b03272dbcecb
Abdalla <abdallag@github.com>
2022-03-29 14:35:04 -0700

    Change port

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '8080'))
     except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+        PORT = 8080
+    app.run(HOST, PORT, debug=True)
