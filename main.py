7f9eb6d67c0ce90844e66faf330bb5c402301cbb
Abdalla <abdallag@github.com>
2022-03-30 15:49:04 -0700

    secrets

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ It contains the definition of routes and views for the application.
 """
 
 from flask import Flask, render_template
+from neo4j import GraphDatabase
+from google.cloud import secretmanager
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -12,8 +14,12 @@ wsgi_app = app.wsgi_app
 
 @app.route('/')
 def hello():
-    """Renders a sample page."""
-    return render_template("main.html")
+    client = secretmanager.SecretManagerServiceClient()
+
+    secret = client.get_secret(
+                        request={"name": f"projects/caip-growth-experiments/secret/neo4j1g"}
+                   )
+    return secret
 
 if __name__ == '__main__':
     import os
